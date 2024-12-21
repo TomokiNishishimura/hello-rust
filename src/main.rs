b0dd6bd0d08e1a5fbbf8d6af13fb2c9d8c429bef
TomokiNishishimura <tomokichigai@gmail.com>
2024-12-21 12:01:33 +0900

    1-1-1 変数と束縛

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, Wasm!");
+  let mut message = "Hello, Wasm!";
+  println!("変数messageに束縛された値: {}", message);
+  message = "Hello, World!";
+  println!("変数messageに束縛された値: {}", message);
 }
