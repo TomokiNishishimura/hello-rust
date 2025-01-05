fa4bb7623a1afd52cfbbf139d87028eb24ac93ba
TomokiNishishimura <tomokichigai@gmail.com>
2025-01-05 15:33:45 +0900

    関数定義

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # hello-rust
+
 hello-rust
+
+## コマンド一覧
+
+### 実行コマンド
+
+```sh
+cargo run
+```
+
+### エラーコード解説
+
+```sh
+rustc --explain E0384
+```
--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,21 @@ fn main() {
   println!("変数messageに束縛された値: {}", message);
   message = "Hello, World!";
   println!("変数messageに束縛された値: {}", message);
+  greeting();
+  greeting2();
+}
+
+// u32型の値を返す関数
+// u32型は符号なし32ビット整数型
+fn greeting() -> u32 {
+  println!("Hello, from WebAssembly!");
+  42
+}
+
+// ユニット型を返す関数
+// ユニット型は意味を持たない値を表す型
+// 返り値がない場合に使われる
+// 省略可能
+fn greeting2() -> () {
+  println!("Hello, from WebAssembly!");
 }
